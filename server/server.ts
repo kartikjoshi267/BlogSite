5708211a06f0e2f0af26b17519549aba7fd3334c
kartikjoshi267 <kartikjoshi267@gmail.com>
2023-10-15 18:57:01 +0530

    Add code for deployment

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import './database';
 import user from './routes/user';
 import blogs from './routes/blogs';
 import cors from 'cors';
+import path from "path";
 
 const app: Express = express();
 const PORT : number = 8000;
@@ -12,9 +13,17 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-app.get('/', (req: any, res: any) => {
-    res.send('Hello World 🚀');
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.get('/', (req: any, res: any) => {
+        res.send('API running successfully');
+    });
+} else {
+    app.use(express.static(path.join(path.resolve(), '../client/dist')));
+    app.get('*', (req: any, res: any) => {
+        res.sendFile(path.resolve(path.resolve(), "../client/dist/index.html"));
+    })
+}
+
 
 app.use('/api/users', user);
 app.use('/api/blogs', blogs);
