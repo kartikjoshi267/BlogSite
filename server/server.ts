461fc19bcde78b24ee057811d791220e350a7055
Kartik <kartikjoshi267@gmail.com>
2024-12-27 14:12:20 +0530

    deployment settings changed in server

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ import './database';
 import user from './routes/user';
 import blogs from './routes/blogs';
 import cors from 'cors';
-import path from "path";
+// import path from "path";
 
 const app: Express = express();
 const PORT : any = process.env.PORT || 8000;
@@ -13,16 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-if (process.env.NODE_ENV !== 'production') {
-    app.get('/', (req: any, res: any) => {
-        res.send('API running successfully');
-    });
-} else {
-    app.use(express.static(path.join(path.resolve(), '../client/dist')));
-    app.get('*', (req: any, res: any) => {
-        res.sendFile(path.resolve(path.resolve(), "../client/dist/index.html"));
-    })
-}
+// if (process.env.NODE_ENV !== 'production') {
+
+app.get('/', (req: any, res: any) => {
+    res.send('API running successfully');
+});
+
+// } else {
+//     app.use(express.static(path.join(path.resolve(), '../client/dist')));
+//     app.get('*', (req: any, res: any) => {
+//         res.sendFile(path.resolve(path.resolve(), "../client/dist/index.html"));
+//     })
+// }
 
 
 app.use('/api/users', user);
